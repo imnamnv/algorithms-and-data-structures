af36e3f977ce2f0700a73566c7a9e27fd877162b
Nam NV <imnamnv@gmail.com>
2023-11-13 18:25:30 +0700

    finished reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reversed = n.toString().split("").reduce((r,char)=>{
+        return char + r;
+    });
+
+    return parseInt(reversed) * Math.sign(n)
+}
 
 module.exports = reverseInt;
+
+
+// function reverseInt(n) {
+//     const reversed = n.toString().split("").reverse().join("");
+
+//     return parseInt(reversed) * Math.sign(n)
+// }
