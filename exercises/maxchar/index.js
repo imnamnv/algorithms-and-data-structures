526339fc64ee064d142e2ff30fe2b151e0aab89b
Nam NV <imnamnv@gmail.com>
2023-11-13 18:54:15 +0700

    finished maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const char = {};
+    let maxNum = "";
+    let maxChar = "";
+    for(const c of str){
+        char[c] = char[c] + 1 || 1
+    }
+
+    for(const c in char){
+        if(char[c] > maxNum){
+            maxNum = char[c];
+            maxChar = c
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
