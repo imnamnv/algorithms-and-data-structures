18a3fcc6ad08368cdf8e4df8c41fc77156f2b075
Nam NV <imnamnv@gmail.com>
2023-11-13 17:35:41 +0700

    finished palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split("").every((char, index)=>{
+        return char === str[str.length - 1 - index]
+    })
+}
 
 module.exports = palindrome;
+
+
+// function palindrome(str) {
+//     const reversed = str.split("").reverse().join("");
+//     return str === reversed
+// }
