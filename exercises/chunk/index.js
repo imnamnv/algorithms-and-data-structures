59c743ae077ad963eb2f674d6eee5472db50fcdc
Nam NV <imnamnv@gmail.com>
2023-11-14 18:43:52 +0700

    finished fixxbuzz, chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,74 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const result = [];
+  let index = 0;
+  while (index < array.length) {
+    result.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return result;
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//     const result = [];
+//     for (let index = 0; index < array.length; index + size) {
+//       let tempArray = [];
+//       if (size >= array.length) {
+//         tempArray = array.splice(index, array.length); //splice will change the array, slice is not
+//       } else {
+//         tempArray = array.splice(index, size);
+//       }
+//       result.push(tempArray);
+//     }
+//     return result;
+//   }
+
+// function chunk(array, size) {
+//     const result = [];
+//     for (let index = 0; index < array.length; index + size) {
+//       let tempArray = [];
+//       if (size >= array.length) {
+//         tempArray = array.splice(index, array.length);
+//       } else {
+//         tempArray = array.splice(index, size);
+//       }
+//       result.push(tempArray);
+//     }
+//     return result;
+//   }
+
+// function chunk(array, size) {
+//     const result = [];
+//     let tempArray = [];
+
+//     for (let index = 0; index < array.length; index++) {
+//       tempArray.push(array[index]);
+
+//       if (tempArray.length === size) {
+//         result.push(tempArray);
+//         tempArray = [];
+//       } else if (index === array.length - 1) {
+//         result.push(tempArray);
+//       }
+//     }
+//     return result;
+//   }
+
+// function chunk(array, size) {
+//   const result = [];
+//   for (const element of array) {
+//     const last = result[result.length - 1]; // get last element of RESULT EVERY LOOP
+//     if (!last || last.length === size) {
+//       result.push([element]);
+//     } else {
+//       last.push(element); // using point function of array
+//     }
+//   }
+
+//   return result;
+// }
